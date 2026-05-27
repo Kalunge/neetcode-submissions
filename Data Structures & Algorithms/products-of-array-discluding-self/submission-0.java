804f35f69a320cc1acb8e6e76c11759911a47e68
Titus Muthomi <muthomititus+github@gmail.com>
2026-05-27 14:55:11 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] prefix = new int[len];
+        int[] post = new int[len];
+
+        prefix[0] = 1;
+        for(int i = 1; i < len; i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+
+        post[len - 1] = 1;
+        for(int i = len - 2; i >= 0; i--){
+            post[i] = post[i + 1] * nums[i + 1];
+        }
+
+        int[] result = new int[len];
+
+        for(int i = 0; i < len; i++){
+            result[i] = prefix[i] * post[i];
+        }
+
+        return result;
+    }
+}  
