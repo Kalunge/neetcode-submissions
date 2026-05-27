aee96c30638aabb1fff1781d18d730c96d8bae9c
Titus Muthomi <muthomititus+github@gmail.com>
2026-05-27 14:40:25 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder builder = new StringBuilder();
+
+        for(String str : strs){
+            builder.append(str.length());
+            builder.append('#');
+            builder.append(str);
+        }
+
+        return builder.toString();
+    }
+
+    public List<String> decode(String str) {
+
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+
+        while(i < str.length()){
+            int j = i + 1;
+            
+            while(str.charAt(j) != '#') j++;
+
+            int len = Integer.parseInt(str.substring(i, j));
+            String word = str.substring(j + 1, j + 1 + len);
+            result.add(word);
+
+            i = j + 1 + len;
+        }
+
+        return result;
+    }
+}
